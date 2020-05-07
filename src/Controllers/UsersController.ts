7a0cc206d5ec8b9ed292943cad97986d32d7cd6a
adryell <adryell.silva10@gmail.com>
2020-05-07 20:20:59 -0300

    feat: added method update and method destroy

--- src/Controllers/UsersController.ts
+++ src/Controllers/UsersController.ts
@@ -1,6 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Response, Request } from 'express'
 import CreateUserService from '../Services/CreateUserService'
 import ListUsersService from '../Services/ListUsersServices'
+import DeleteUserService from '../Services/DeleteUserService'
+import UpdateUserService from  '../Services/UpdateUserService'
 
 class UsersController {
   public async store(request: Request, response: Response): Promise<Response> {
@@ -23,10 +26,26 @@ class UsersController {
 
   public async show(request: Request, response: Response): Promise<Response> {
     const listUser = new ListUsersService()
-
+    
     const user = await listUser.userById(request.params.id)
+    
+    return response.json(user)
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateUser = new UpdateUserService()
+
+    const user = await updateUser.execute(request.body, request.params.id)
 
     return response.json(user)
+  }
+
+  public async destroy(request: Request, response: Response): Promise<Response> {
+    const deleteUser = new DeleteUserService()
+
+    await deleteUser.execute(request.params.id)
+
+    return response.json('User excluido')
   }
 }
 
