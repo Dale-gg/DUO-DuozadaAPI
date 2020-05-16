53456b22c5584568333ff053605e41fbc3789211
João Lenon <lenonsec7@gmail.com>
2020-05-16 00:05:55 -0300

    feat: CI Confs

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,16 @@
 module.exports = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ["./src/App/Models/*.ts"],
-  migrations: ["./src/Database/migrations/*.ts"],
+  entities: ['./src/App/Models/*.ts', './src/App/Models/*.js'],
+  migrations: [
+    './src/Database/migrations/*.ts',
+    './src/Database/migrations/*.js',
+  ],
   cli: {
-    migrationsDir: "./src/Database/migrations"
-  }
+    migrationsDir: './src/Database/migrations',
+  },
 }
