d1464078b9b995a1dfc4d86a8e9e9b6e612616f2
João Lenon <lenonsec7@gmail.com>
2020-05-25 23:30:43 -0300

    feat: ILane Interface

---
+++ src/App/Interfaces/ILane.ts
@@ -0,0 +1,12 @@
+export default interface ILane {
+  name?: string
+  icons?: string[]
+}
+
+export interface ILaneObject {
+  id: string
+  name: string
+  icons: string[]
+  created_at: Date
+  updated_at: Date
+}
