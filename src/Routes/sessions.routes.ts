c4654092d6a29686f8f1ffca9b15b3cf3b77f08e
João Lenon <lenonsec7@gmail.com>
2020-06-02 11:48:02 -0300

    feat: sessions routes

--- src/Routes/sessions.routes.ts
+++ src/Routes/sessions.routes.ts
@@ -1,22 +1,9 @@
 import { Router } from 'express'
-
-import AuthenticateUserService from '../App/Services/AuthenticateUserService'
+import SessionsController from '../App/Controllers/SessionsController'
 
 const sessionsRouter = Router()
+const sessionController = new SessionsController()
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body
-
-  const authenticateUser = new AuthenticateUserService()
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  })
-
-  delete user.password
-
-  return response.json({ user, token })
-})
+sessionsRouter.post('/', sessionController.store)
 
 export default sessionsRouter
