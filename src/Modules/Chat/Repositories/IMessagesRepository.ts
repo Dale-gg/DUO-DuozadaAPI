5e56a265d7eeabc99026c92020ec2862e2749719
João Lenon <lenonsec7@gmail.com>
2020-08-16 00:13:44 -0300

    feat: Create Messages with WS Protocol

--- src/Modules/Chat/Repositories/IMessagesRepository.ts
+++ src/Modules/Chat/Repositories/IMessagesRepository.ts
@@ -2,7 +2,7 @@ import Message from '@Modules/Chat/Infra/Typeorm/Entities/Message'
 import ICreateMessageDTO from '@Modules/Chat/Dtos/ICreateMessageDTO'
 
 export default interface IChatsRepository {
-  all(): Promise<Message[]>
+  all(chat_id: string): Promise<Message[]>
   create(data: ICreateMessageDTO): Promise<Message>
   save(message: Message): Promise<Message>
 }
