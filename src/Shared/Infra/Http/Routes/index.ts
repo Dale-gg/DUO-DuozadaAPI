ceb25ff202ad67fe0ccd186f8f6cdc6dc070020a
João Lenon <lenonsec7@gmail.com>
2020-08-16 00:02:00 -0300

    feat: Chat Module

--- src/Shared/Infra/Http/Routes/index.ts
+++ src/Shared/Infra/Http/Routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import sessionsRouter from '@Modules/Users/Infra/Http/Routes/sessions.routes'
+import chatsRouter from '@Modules/Chat/Infra/Http/Routes/chats.routes'
 import usersRouter from '@Modules/Users/Infra/Http/Routes/users.routes'
 import passwordRouter from '@Modules/Users/Infra/Http/Routes/password.routes'
 import profileRouter from '@Modules/Users/Infra/Http/Routes/profile.routes'
@@ -10,6 +11,7 @@ import dislikeRouter from '@Modules/LikeDislike/Infra/Http/Routes/dislike.routes
 const routes = Router()
 
 routes.use('/sessions', sessionsRouter)
+routes.use('/chats', chatsRouter)
 routes.use('/users', usersRouter)
 routes.use('/password', passwordRouter)
 routes.use('/profile', profileRouter)
