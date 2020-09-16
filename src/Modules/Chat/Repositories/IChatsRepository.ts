9b472797658afcbc5da17040c1db04cd28d2003a
João Lenon <lenonsec7@gmail.com>
2020-09-16 01:30:00 -0300

    feat: Refactor, chat by user_id

--- src/Modules/Chat/Repositories/IChatsRepository.ts
+++ src/Modules/Chat/Repositories/IChatsRepository.ts
@@ -3,7 +3,7 @@ import ICreateChatDTO from '@Modules/Chat/Dtos/ICreateChatDTO'
 
 export default interface IChatsRepository {
   all(user_id: string): Promise<Chat[]>
-  findById(id: string): Promise<Chat | undefined>
+  findById(user_id: string, id: string): Promise<Chat | undefined>
   create(data: ICreateChatDTO): Promise<Chat>
   save(chat: Chat): Promise<Chat>
 }
